33569f6c875c127dbf31700d12eb6f75e0043463
cyrillaebische <cyrillaebische@hes-so.ch>
2025-12-04 11:00:31 +0100

    matched colors to inequality for 2022 and Sans position de cadre

--- src/views/map_view.py
+++ src/views/map_view.py
@@ -1,6 +1,7 @@
 from dash import html, dcc, Input, Output
 import dash_mantine_components as dmc
 import geopandas as gpd
+import pandas as pd
 
 from views.view import View
 
@@ -8,8 +9,19 @@ from common.constants import ComponentIds, ContainerIds
 import common.components as components
 import geopandas as gpd
 geofile_path = "2025_GEOM_TK/03_ANAL/Gesamtfläche_gf/K4_greg20001205_gf/K4greg20001205gf_ch2007Poly.shp"
+dfinq = pd.read_csv('data/inequality_by_region_position_year.csv')
+dfinq_filtered = dfinq.loc[(dfinq["Year"] == 2022) & (dfinq["Position"] == "Sans fonction de cadre")]
 gdf = gpd.read_file(geofile_path)
-m = gdf.explore('id')
+# print(gdf.columns)
+# print(dfinq_filtered.columns)
+# print(gdf['name'].head())
+# print(dfinq_filtered['Region'].head())
+gdf = gdf.merge(dfinq_filtered[['Region', 'Inequality']], left_on='name', right_on='Region', how='left')
+gdf = gdf.drop(columns=['id', 'name'])
+m = gdf.explore(column='Inequality',
+                cmpa='Reds',
+                legend=True,
+                legend_kwds={'lable': 'Inequality level'})
 m.save("map.html")
 
 class MapView(View):
