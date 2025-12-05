bbaab8bd368303ae0e6c8544b3d7d5bf9dd92868
wilfried01 <ngueukam.karel@stormit.ch>
2025-12-05 12:19:31 +0100

    [WIP] Adding maps on app view

--- src/views/map_view.py
+++ src/views/map_view.py
@@ -6,6 +6,7 @@ import plotly.express as px
 import geopandas as gpd
 import json
 from geojson_rewind import rewind
+import os
 
 from views.view import View
 
@@ -32,8 +33,9 @@ class MapView(View):
     
 
     def renderMap(self, year):
+        data_path_file = os.path.join(os.getcwd(), 'data', '12-22_suisse_info_tout_tout.csv')
         try:
-            df = pd.read_csv('data.csv', sep=';', encoding='latin-1')
+            df = pd.read_csv(data_path_file, sep=';', encoding='latin-1')
         except Exception as e:
             print(f"Erreur lors du chargement du fichier: {e}")
             # Si le chargement échoue, nous arrêtons ici.
@@ -46,7 +48,7 @@ class MapView(View):
         df_for_each_region = {}
 
         for region in allowed_regions:
-            # Filtrer le DataFrame principal pour la région actuelle et créer une copie propre (.copy())
+            # Filtrer le DataFrame principal pour la région actuelle et créer une copie propre
             df_for_each_region[region] = df[df['Grande région'] == region].copy()
 
         df_pay_gap_by_region = {}
@@ -75,7 +77,6 @@ class MapView(View):
             df_mean = df_region.groupby(['Année', 'Grande région'])['Pay_Gap'].mean().round(2).reset_index()
             df_mean.rename(columns={'Pay_Gap': 'Pay_Gap_Mean'}, inplace=True)
             dfs_pay_gap_mean_by_region_year.append(df_mean)
-            print(f"✅ Moyenne calculée pour la région {region}. Taille du DataFrame: {df_mean.shape}")
 
         df_pay_gap = pd.concat(dfs_pay_gap_mean_by_region_year, ignore_index=True)
 
@@ -89,9 +90,36 @@ class MapView(View):
 
         df_to_print = {}
         for annee, df_annee in df_by_year.items():
-            region_pay_gap = df_annee[['Grande région', 'Pay_Gap_Mean']].copy()
-            df_to_print[annee] = region_pay_gap.set_index('Grande région')['Pay_Gap_Mean']
+            df_annee.rename(columns={'Grande région': 'REGCH_fr'}, inplace=True) 
+            region_pay_gap = df_annee[['REGCH_fr', 'Pay_Gap_Mean']].copy()
+            df_to_print[annee] = region_pay_gap.set_index('REGCH_fr')['Pay_Gap_Mean']
 
+        swiss_geo_path_file = os.path.join(os.getcwd(), 'data', 'swiss_geojson.geojson')
+        with open(swiss_geo_path_file, encoding='utf-8') as file:
+            reg_ch = json.load(file)
 
+        reg_ch = rewind(reg_ch, rfc7946=False)
+
+        geo_df = gpd.GeoDataFrame.from_features(
+            reg_ch['features']
+            
+        ).merge(df_to_print[2016], on='REGCH_fr').set_index('REGCH_fr')
+
+        fig = px.choropleth(geo_df, geojson=geo_df.geometry, color='Pay_Gap_Mean', 
+                            locations=geo_df.index,
+                            color_continuous_scale="Viridis",
+                            range_color=(0, geo_df['Pay_Gap_Mean'].max()),
+                            projection="mercator")
+
+        fig.update_geos(fitbounds="locations", visible=False) 
+        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        # fig.show() 
         # TODO replace by actual map
+
+        # print(os.getcwd())
+        # swiss_geo_path_file = os.path.join(os.getcwd(), 'data', 'swiss_geojson.geojson')
+        # with open(swiss_geo_path_file, encoding='utf-8') as file:
+        #     reg_ch = json.load(file)
+
+        # print(reg_ch)
         return html.Div(f"Carte des écarts salariaux de l'année {year}")
