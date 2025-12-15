2706b7fe9abec4b52028b2bff45e7ae4f1519f5a
wilfried01 <ngueukam.karel@stormit.ch>
2025-12-15 19:04:38 +0100

    Resolving path independantly from os and doing some cleaning

--- src/views/comparisons_view.py
+++ src/views/comparisons_view.py
@@ -1,17 +1,24 @@
-from dash import html, dcc, Input, Output
+from dash import html, dcc
 import dash_mantine_components as dmc
 
 from views.view import View
 
-from common.constants import ComponentIds, ContainerIds, Series
+from common.constants import ContainerIds, Series
 import common.components as components
 
 import pandas as pd
 import plotly.graph_objects as go
 
+import os
 
-COMPARISON_DATA = pd.read_csv('data/12-22_suisse_sect26_tout_tout.csv', sep=';')
-# columns: "Année";"Grande région";"Division économique";"Position professionnelle";"Sexe";"Médiane";"P10";"P25";"P75";"P90"
+data_path_file = os.path.join(
+    os.getcwd(), 'data', '12-22_suisse_sect26_tout_tout.csv'
+)
+
+COMPARISON_DATA = pd.read_csv(data_path_file, sep=';')
+# columns: "Année";"Grande région";"Division économique";
+# "Position professionnelle";"Sexe";"Médiane";"P10";"P25";"P75";"P90"
+
 
 class ComparisonsView(View):
 
@@ -19,41 +26,41 @@ class ComparisonsView(View):
     label = "Comparaison des salaires"
     icon = "tabler:scale"
 
-
     def render(self):
         return dmc.Stack(
             children=[
                 html.H3(self.label),
-
                 components.seriesSelector(),
-
                 html.Div(id=ContainerIds.REGION_OR_POSITION_CONTAINER),
-                
                 components.dataContainer(id=ContainerIds.COMPARISON_CONTAINER),
-
                 components.yearSelector(),
             ],
         )
-    
-    
+
     def renderComparison(self, series, region, position, year):
         mask = COMPARISON_DATA["Année"] == year
 
         if series == Series.POSITION:
-            mask = mask & (COMPARISON_DATA["Position professionnelle"] == position)
-            title = f"Comparaison des salaires {year} de la position professionnelle : {position}"
+            mask = mask & (
+                COMPARISON_DATA["Position professionnelle"] == position
+            )
+            title = (
+                f"Comparaison des salaires {year} de la position "
+                f"professionnelle : {position}"
+            )
             series_name = "Grande région"
         else:
             mask = mask & (COMPARISON_DATA["Grande région"] == region)
             title = f"Comparaison des salaires {year} de la région : {region}"
             series_name = "Position professionnelle"
 
-        # columns: "Année";"Grande région";"Division économique";"Position professionnelle";"Sexe";"Médiane";"P10";"P25";"P75";"P90"
+        # columns: "Année";"Grande région";"Division économique";
+        # "Position professionnelle";"Sexe";"Médiane";"P10";"P25";"P75";"P90"
         full_df = COMPARISON_DATA[mask]
 
         def getValues(df, column):
             return df[column].tolist()
-        
+
         color = {
             "Hommes": "darkblue",
             "Femmes": "tomato"
@@ -76,8 +83,8 @@ class ComparisonsView(View):
                 showlegend=True,
                 hoverinfo="y",
             ))
-        
-        # TODO ajotuer séparation entre Suisse et autres régions / entre Toutes positions prof et les autres
+        # TODO ajotuer séparation entre Suisse et autres régions /
+        # entre Toutes positions prof et les autres
 
         fig.update_layout(
             boxmode='group',
@@ -89,4 +96,3 @@ class ComparisonsView(View):
         return html.Div(children=[
             graph,
             ])
-    
